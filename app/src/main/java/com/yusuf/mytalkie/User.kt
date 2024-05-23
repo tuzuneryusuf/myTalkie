87d8e8204c0c9580db3767ee4845181dd93eaae6
Yusuf <yusuf.tuzuner@boun.edu.tr>
2024-05-23 17:44:31 +0300

    finishh

---
+++ app/src/main/java/com/yusuf/mytalkie/User.kt
@@ -0,0 +1,16 @@
+package com.yusuf.mytalkie
+
+
+    class User {
+    var name: String? = null
+    var email: String? = null
+    var uid: String? = null   //unique id for user
+
+    constructor(){}
+    // ? means that nullable
+    constructor(name: String?, email: String?, uid: String?){
+        this.name = name
+        this.email = email
+        this.uid = uid
+    }
+}
